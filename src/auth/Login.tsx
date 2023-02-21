06e247a936f43a1acae7871ab168f818356af864
fahimshah36 <fahimshah860@gmail.com>
2023-02-21 11:39:53 +0600

    mixed content error warning message on login

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -5,6 +5,7 @@ import logo from "../components/assets/logo.png";
 import {ILoginType} from "./authTypes";
 import {useLoginMutation} from "./loginApi";
 import {useEffect} from "react";
+import {Alert} from "antd";
 
 function Login() {
   const navigate = useNavigate();
@@ -24,57 +25,72 @@ function Login() {
     await login(body);
   };
   return (
-    <Row className="login-page">
-      <Form initialValues={{remember: true}} onFinish={onFinish}>
-        <Card
-          style={{
-            borderRadius: "8px",
-            marginTop: "1rem",
-            boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
-          }}
-        >
-          <Col xs={24} sm={24} md={24} lg={6}>
-            <img
-              className="illustration-wrapper"
-              src={logo}
-              alt="Logo"
-              width={250}
-            />
-          </Col>
-
-          <Form.Item
-            style={{marginTop: "20px"}}
-            name="email"
-            rules={[{required: true, message: "Please input your username!"}]}
+    <>
+      <Row className="login-page">
+        <Form initialValues={{remember: true}} onFinish={onFinish}>
+          <Card
+            style={{
+              borderRadius: "8px",
+              marginTop: "1rem",
+              boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+            }}
           >
-            <Input type="email" placeholder="Username" />
-          </Form.Item>
+            <Col xs={24} sm={24} md={24} lg={6}>
+              <img
+                className="illustration-wrapper"
+                src={logo}
+                alt="Logo"
+                width={250}
+              />
+            </Col>
 
-          <Form.Item
-            name="password"
-            rules={[{required: true, message: "Please input your password!"}]}
-          >
-            <Input.Password placeholder="Password" />
-          </Form.Item>
-
-          {/* <Form.Item name='remember' valuePropName='checked'>
-      <Checkbox />
-      <Typography.Text>Remember me</Typography.Text>
-    </Form.Item> */}
-          <Row justify={"center"}>
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="login-form-button"
-              >
-                LOGIN
-              </Button>
+            <Form.Item
+              style={{marginTop: "20px"}}
+              name="email"
+              rules={[{required: true, message: "Please input your username!"}]}
+            >
+              <Input type="email" placeholder="Username" />
             </Form.Item>
+
+            <Form.Item
+              name="password"
+              rules={[{required: true, message: "Please input your password!"}]}
+            >
+              <Input.Password placeholder="Password" />
+            </Form.Item>
+
+            <Row justify={"center"}>
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                >
+                  LOGIN
+                </Button>
+              </Form.Item>
+            </Row>
+          </Card>
+          <Row style={{marginTop: "1rem"}}>
+            <Alert
+              message="Warning"
+              description={
+                <>
+                  This website is loaded over HTTPS but the api request is HTTP,
+                  <br />
+                  so it throws an error from the browser saying Mixed Content.
+                  <br />
+                  To Log in you will have to allow Insecure Content
+                  <br /> by going to site settings from the browser
+                </>
+              }
+              type="warning"
+              showIcon
+            />
           </Row>
-        </Card>
-      </Form>
-    </Row>
+        </Form>
+      </Row>
+    </>
   );
 }
 
